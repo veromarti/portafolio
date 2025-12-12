eab0de33ee5bcf16abd16366e1302dea8b2f2f85
Veronica Martinez Cadavid <veromar.c96@gmail.com>
2025-12-11 20:53:03 -0500

    feature projects

---
+++ script.js
@@ -0,0 +1,27 @@
+const pythonButton = document.getElementById('py-button')
+pythonButton.addEventListener('click', ()=>{
+    const window = document.getElementById('window-frame')
+    const route = document.getElementById('projects-window')
+    if (window.style.visibility === 'visible' && route.getAttribute('src') === 'assets/python-projects.html'){
+        window.style.visibility = 'hidden'
+        return false
+    }
+    
+    route.src = 'assets/python-projects.html'
+    window.style.visibility = 'visible'
+})
+
+const cssButton = document.getElementById('css-button')
+cssButton.addEventListener('click', ()=>{
+    const window = document.getElementById('window-frame')
+    const route = document.getElementById('projects-window')
+
+    if (window.style.visibility === 'visible' && route.getAttribute('src') === 'assets/html-css.html'){
+        window.style.visibility = 'hidden'
+        console.log("hwew",window)
+        return false
+    }
+    
+    route.src = 'assets/html-css.html'
+    window.style.visibility = 'visible'
+})
