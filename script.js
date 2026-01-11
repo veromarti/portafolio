11e67fb76878152916435a3da2879b40e0f9bd2a
Veronica Martinez Cadavid <veromar.c96@gmail.com>
2026-01-11 16:19:22 -0500

    added js projects

--- script.js
+++ script.js
@@ -25,3 +25,18 @@ cssButton.addEventListener('click', ()=>{
     route.src = 'assets/html-css.html'
     window.style.visibility = 'visible'
 })
+
+const jsButton = document.getElementById('js-button')
+jsButton.addEventListener('click', ()=>{
+    const window = document.getElementById('window-frame')
+    const route = document.getElementById('projects-window')
+
+    if (window.style.visibility === 'visible' && route.getAttribute('src') === 'assets/javascript.html'){
+        window.style.visibility = 'hidden'
+        console.log("hwew",window)
+        return false
+    }
+    
+    route.src = 'assets/javascript.html'
+    window.style.visibility = 'visible'
+})
